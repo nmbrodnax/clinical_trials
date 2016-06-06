e83acf74213c892637a37eab05b77d00210a0df0
NaLette Brodnax <brodnax_nalette_m@lilly.com>
2016-06-06 16:42:51 -0400

    parse xml

--- read_xml.R
+++ read_xml.R
@@ -1,2 +1,10 @@
+# Load XML file into memory and parse into r object
 library(XML)
-xml.url <- "telemed_trials_may30.xml"
+xml.url <- "telemed_trials_may30.xml" #loads in the xml file
+xmlfile <- xmlTreeParse(xml.url) #converts the xml file to an r object
+xmltop <- xmlRoot(xmlfile) #root is the highest level in the hierarchy
+
+# Convert xml object into data frame
+trials <- xmlSApply(xmltop, function(x) xmlSApply(x,xmlValue))
+trials_df <- data.frame(t(trials), row.names=NULL)
+#this didn't quite work, need to fix
