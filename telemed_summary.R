5509a0d5ffc01649c4587dfb4e16687c4b1efa99
NaLette Brodnax <brodnax_nalette_m@lilly.com>
2016-06-08 13:22:16 -0400

    converting string to factor var

--- telemed_summary.R
+++ telemed_summary.R
@@ -1,1 +1,9 @@
+library(ggplot2)
+library(ggthemes)
+
 load("telemed_trials.RData")
+trials$Target_size <- as.numeric(trials$Target_size)
+trials$Study_type <- as.factor(trials$Study_type)
+trials$Phase <- as.factor(trials$Phase)
+trials$Countries <- as.factor(trials$Countries)
+trials$Source_Register <- as.factor(trials$Source_Register)
