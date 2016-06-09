6a2a3f4a51c77c6d9e8fe884c2e73f49c6882547
NaLette Brodnax <brodnax_nalette_m@lilly.com>
2016-06-09 10:31:59 -0400

    xml to dictionary

--- read_xml.py
+++ read_xml.py
@@ -1,5 +1,18 @@
 # Import an XML document and parse tree into python objects
 
-import xml.etree.ElementTree as ET
-tree = ET.parse('telmed_trials_may30.xml')
+import xml.etree.ElementTree
+tree = xml.etree.ElementTree.parse('telemed_trials_may30.xml')
 root = tree.getroot()
+
+all_trials = []
+i = 0
+j = 0
+
+while i < len(root):
+    trial = {}
+    while j < len(root[i]):
+        trial[root[i][j].tag] = root[i][j].text
+        j += 1
+    all_trials.append(trial)
+    i += 1
+
