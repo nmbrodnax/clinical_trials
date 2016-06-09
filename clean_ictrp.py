f685c7629b077d4c1f02f304e9b7a3a68e9c2f3f
NaLette Brodnax <brodnax_nalette_m@lilly.com>
2016-06-09 11:02:09 -0400

    lists of sponsor types

---
+++ clean_ictrp.py
@@ -0,0 +1,16 @@
+#Functions for cleaning the ICTRP data
+
+hospital_terms = ["hospital", "medical center", "health center", "clinic",
+                  "hopitaux", "hospitalier"]
+
+academic_terms = ["university", "college", "universitas", "universidad"]
+
+government_terms = ["department", "centers for disease control", "authority",
+                    "va office", "government funding body"]
+
+ngo_terms = ["institute", "institut", "institulet","center", "centre",
+             "foundation", "fundacion", "fondazione", "association",
+             "associates", "academy"]
+
+commercial = ["AbbVie", "Sanofi", "Pfizer", "Eli Lilly", "Bayer", "Novartis",
+              "GlaxoSmithKline", "Merck"]
