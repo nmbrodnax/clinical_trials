e0724b027abcde807ccb86fb8b42b2f87cd43996
NaLette Brodnax <brodnax_nalette_m@lilly.com>
2016-06-10 13:21:52 -0400

    dict of sponsor categories

--- clean_ictrp.py
+++ clean_ictrp.py
@@ -1,16 +1,39 @@
-#Functions for cleaning the ICTRP data
+# Functions for cleaning the ICTRP data
 
-hospital_terms = ["hospital", "medical center", "health center", "clinic",
-                  "hopitaux", "hospitalier"]
 
-academic_terms = ["university", "college", "universitas", "universidad"]
+def main():
+    hospital_terms = ["hospital", "medical center", "health center", "clinic",
+                      "hopitaux", "hospitalier"]
+    academic_terms = ["university", "college", "universitas", "universidad"]
+    government_terms = ["department", "centers for disease control",
+                        "authority", "va office", "government funding body"]
+    ngo_terms = ["institute", "institut", "institulet", "center", "centre",
+                 "foundation", "fundacion", "fondazione", "association",
+                 "associates", "academy"]
+    commercial_terms = ["AbbVie", "Sanofi", "Pfizer", "Eli Lilly", "Bayer",
+                        "Novartis", "GlaxoSmithKline", "Merck"]
 
-government_terms = ["department", "centers for disease control", "authority",
-                    "va office", "government funding body"]
+    categories = [
+        (hospital_terms, "Hospital"),
+        (academic_terms, "University"),
+        (government_terms, "Government"),
+        (ngo_terms, "NGO"),
+        (commercial_terms, "Commercial")
+    ]
 
-ngo_terms = ["institute", "institut", "institulet","center", "centre",
-             "foundation", "fundacion", "fondazione", "association",
-             "associates", "academy"]
+    sponsor_categories = {}
+    for pair in categories:
+        for term in pair[0]:
+            sponsor_categories[term] = pair[1]
 
-commercial = ["AbbVie", "Sanofi", "Pfizer", "Eli Lilly", "Bayer", "Novartis",
-              "GlaxoSmithKline", "Merck"]
+    print(sponsor_categories)
+
+
+# def sponsor_category(text, category_dict):
+#     """"Classifies sponsor information into one of six categories
+#         str --> str"""
+#     pass
+
+
+if __name__ == '__main__':
+    main()
