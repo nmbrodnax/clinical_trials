7105cc85d595af97b9d0c01a83a078f5a7b3223f
NaLette Brodnax <brodnax_nalette_m@lilly.com>
2016-06-13 15:48:37 -0400

    export to csv

--- read_xml.R
+++ read_xml.R
@@ -3,3 +3,4 @@ library(XML)
 xml.url <- "telemed_trials_may30.xml" #loads in the xml file
 trials <- xmlToDataFrame(xml.url)
 save(trials, file = "telemed_trials.RData")
+write.table(trials, file = "telemed_trials.csv", sep = ",", row.names = FALSE)
