e8ab28fa3b9c917cb7a46fb410e2fc1af2473fe4
NaLette Brodnax <brodnax_nalette_m@lilly.comm>
2016-07-19 10:50:16 -0400

    map color and remove axes

--- trial_map.R
+++ trial_map.R
@@ -34,10 +34,20 @@ ggsave("global_trial_map_continuous.pdf")
 # create plot with map of trial data in discrete buckets
 lilly <- c("#EAE5E3", "#D3CCC8", "#A59D95", "#FFE789", "#FFDC45", "#FF9352",
            "#F36A50", "#D52B1E")
-trial_group <- cut(trial_data$trials, seq(1,600, by = 25))
-ggplot(trial_data, aes(map_id = country)) +
+categories <- c("0" ,"1-25", "26-50", "51-75", "76-100", "101-125", "126-150", "151-175",
+                "176-200", "201-225", "226-250", "251-275", "276-300", "301-325", "326-350", 
+                "351-375", "376-400", "401-425", "426-450", "451-475", "476-500", "501-525", 
+                "526-550", "551-575", "576-600")
+cat_subset <- c("1-25", "26-50", "51-75", "76-100", "101-125", "201-225", "326-350",
+                "451-475", "501-525")
+trial_group <- cut(trial_data$trials, seq(0,600, by = 25))
+ggplot(trial_data, aes(map_id = country), na.omit()) +
   geom_map(aes(fill = trial_group), map = worldmap) +
   expand_limits(x = worldmap$long, y = worldmap$lat) +
-  theme(legend.title = element_blank(), legend.position = "bottom") +
-  scale_fill_discrete(h = c(0,280), h.start = 250, direction = 1)
+  theme(legend.title = element_blank(), legend.position = "bottom", 
+        axis.title.x=element_blank(), axis.title.y=element_blank(),
+        axis.text.x=element_blank(), axis.text.y=element_blank(), 
+        axis.ticks=element_blank()) +
+  scale_fill_discrete(h = c(0,135), h.start = 0, direction = 1, labels = cat_subset,
+                      na.value = "#A59D95")
 ggsave("global_trial_map_discrete.pdf")
