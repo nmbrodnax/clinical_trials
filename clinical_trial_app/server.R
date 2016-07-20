095f29d02e2ac0b6cde9b7e2bd3b7376fa7efdf1
NaLette Brodnax <brodnax_nalette_m@lilly.comm>
2016-07-20 18:24:55 -0400

    tweaking text formatting

--- clinical_trial_app/server.R
+++ clinical_trial_app/server.R
@@ -14,26 +14,41 @@ shinyServer(function(input, output) {
         theme(legend.title = element_blank(), legend.position = "top", 
               axis.title.x=element_blank(), axis.title.y=element_blank(),
               axis.text.x=element_blank(), axis.text.y=element_blank(), 
-              axis.ticks=element_blank()) +
+              axis.ticks=element_blank(), plot.title = element_text(size = 20)) +
         guides(colour = guide_legend(nrow = 1)) +
         scale_fill_discrete(h = c(0,135), h.start = 0, direction = 1, labels = cat_subset,
                             na.value = "#A59D95")
     })
     output$plot1 <- renderPlot({
-      (ggplot(trials[which(trials$sponsor == input$sponsor & trials$area != 'NA'),], aes(area, fill=type))
+      (ggplot(trials[which(trials$sponsor == input$sponsor & trials$area != 'NA'),], 
+              aes(area, fill=type))
        + ylab('Number of Trials')
        + xlab('Therapeutic Area')
+       + theme(axis.text.x = element_text(angle = 90, size = 14, hjust = 1),
+               axis.text.y = element_text(size = 14),
+               axis.title.x = element_text(size = 16),
+               axis.title.y = element_text(size = 16),
+               plot.title = element_text(size = 18),
+               legend.position = "top")
        + ggtitle('Clinical Trials by Therapeutic Area')
+       + guides(fill=guide_legend(title = "Type"), colour = guide_legend(nrow = 1))
        + geom_bar()
-       + coord_flip())
+       )
     })
     output$plot2 <- renderPlot({
       (ggplot(trials[which(trials$area == input$area & !is.na(trials$phase)),], 
               aes(phase, fill=sponsor))
        + ylab('Number of Trials')
        + xlab('Phase')
+       + theme(axis.text.x = element_text(size = 14, hjust = 1),
+               axis.text.y = element_text(size = 14),
+               axis.title.x = element_text(size = 16),
+               axis.title.y = element_text(size = 16),
+               plot.title = element_text(size = 18),
+               legend.position = "top")
        + ggtitle('Clinical Trials by Phase')
        + geom_bar()
+       + guides(fill=guide_legend(title = "Sponsor"))
        )
     })
 })
